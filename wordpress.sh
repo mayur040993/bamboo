fbebf2a975a4068db1df2392d7038c96f0aa2da1
mayur <rastogi.mayur04@gmail.com>
2016-04-16 17:30:15 +0530

    chnges wp

--- wordpress.sh
+++ wordpress.sh
@@ -2,7 +2,7 @@
 S_TIME=`date +%H:%M:%S`
 echo "Installing wordpress"
 sudo apt-get update
-echo "hello122"
+echo "hello1ds2"
 echo "---------- intsalling Nginx -------------------"
 sudo apt-get -y install nginx
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password igdefault'
