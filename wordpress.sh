3a5c683d02b85c80d9a7477219b64e4f10bc0970
mayur <rastogi.mayur04@gmail.com>
2016-04-16 16:39:22 +0530

    edit wp sh file

--- wordpress.sh
+++ wordpress.sh
@@ -2,6 +2,7 @@
 S_TIME=`date +%H:%M:%S`
 echo "Installing wordpress"
 sudo apt-get update
+echo "hello"
 echo "---------- intsalling Nginx -------------------"
 sudo apt-get -y install nginx
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password igdefault'
